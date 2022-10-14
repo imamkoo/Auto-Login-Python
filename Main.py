a6e67b20239b094db2f2f7fc7115dda51da9fc43
Imam Kholid Ubaidillah <95548041+imamkoo@users.noreply.github.com>
2022-10-15 05:05:50 +0700

    Update Main.py
    delete

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 username = "imam.ubaidillah@binus.ac.id" 
-password = "AllahSWT."
+password = ""
 
 driver = webdriver.Chrome("E:/@BINUS OL/PYTHON/chromedriver")
 
